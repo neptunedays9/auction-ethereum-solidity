df9952867c1fa8aa13b6a7a796f1db3c430a1415
Neptune <neptunedays9@gmail.com>
2020-05-17 15:07:55 +1000

    Added bid function

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -6,7 +6,7 @@ contract Auction {
         uint id;
         string name;
         uint maxBidAmount;
-        uint maxBidPartyId;
+        address maxBidPartyId;
     }
 
     mapping(uint => Item) public items;
@@ -15,10 +15,18 @@ contract Auction {
 
     function addItem (string memory _name) private {
         itemsCount++;
-        items[itemsCount] = Item(itemsCount, _name, 0, 0);
+        items[itemsCount] = Item(itemsCount, _name, 0, address(0x0));
     }
 
     constructor() public {
         addItem('Pembroke 38, Epping 2121');
+    }
+
+    function bid(uint _itemId, uint bidAmount) public {
+
+        // add the check, only if the amount is more, assign it
+        items[_itemId].maxBidAmount = bidAmount;
+        items[_itemId].maxBidPartyId = msg.sender;
+
     }
 }
