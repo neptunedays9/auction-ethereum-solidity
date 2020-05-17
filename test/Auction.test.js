83d03283884acd6287563a1169ed24640704f852
Neptune <neptunedays9@gmail.com>
2020-05-17 19:11:22 +1000

    Added test for bidding

--- test/Auction.test.js
+++ test/Auction.test.js
@@ -1,6 +1,8 @@
 var Auction = artifacts.require('./Auction.sol')
 
 contract("Auction", (accounts) => {
+    var auctionInstance;
+
     it("Initializes with an item", () => {
         return Auction.deployed().then((instance) => {
             return instance.itemsCount();
@@ -21,5 +23,22 @@ contract("Auction", (accounts) => {
             assert.equal(item[1], 'Pembroke 38, Epping 2121');
         });
 
+    });
+
+    it("should allow the party to bid", () => {
+        var itemId;
+        var bidAmount = 1000;
+        return Auction.deployed().then((i) => {
+            auctionInstance = i;
+            itemId = 1;
+        }).then(() => { 
+            return web3.eth.getAccounts().then((acc) => {
+                return auctionInstance.bid(itemId, bidAmount, {from : acc[0]})
+            }).then((txn) => {
+                 return auctionInstance.items(itemId)
+            }).then((item) => {
+                 assert.equal(item[2], 1000, "bidAmount is updated")
+            })
+        })
     });
 });
